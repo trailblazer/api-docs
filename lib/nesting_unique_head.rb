8fc8db8f3f7fd05f007602f08141cf9af1f5be45
Nick Sutterer <apotonick@gmail.com>
2018-01-26 22:45:32 +0100

    always create two-segment header anchors, e.g. operation-call, and not operation-api-call.

--- lib/nesting_unique_head.rb
+++ lib/nesting_unique_head.rb
@@ -11,10 +11,14 @@ class NestingUniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
     friendly_text = text.parameterize
     @@headers_history[header_level] = text.parameterize
 
+    # pp @@headers_history
     if header_level > 1
-      for i in (header_level - 1).downto(1)
-        friendly_text.prepend("#{@@headers_history[i]}-") if @@headers_history.key?(i)
-      end
+      # always have two levels, only: operation-call, instead of operation-api-call
+
+      friendly_text = [ @@headers_history[1], friendly_text ].join("-")
+      # for i in (header_level - 1).downto(1)
+        # friendly_text.prepend("#{@@headers_history[0]}-") if @@headers_history.key?(0)
+      # end
     end
 
     return "<h#{header_level} id='#{friendly_text}'>#{text}</h#{header_level}>"
