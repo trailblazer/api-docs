f9306a6f6519906be523c2db677640ab6779d63b
Nick Sutterer <apotonick@gmail.com>
2018-01-27 16:38:04 +0100

    test OP.

--- lib/torture_helper.rb
+++ lib/torture_helper.rb
@@ -1,11 +1,11 @@
 require "torture/snippet"
 
 module TortureHelper
-  def snippet(component, section, hide:nil)
+  def snippet(component, section, hide:nil, file:nil)
     config = current_page.data["torture"]
     config = config[component]
 
-    file = config [:default_file]
+    file ||= config[:default_file]
     root = config[:root] or raise "No root found for #{component}"
 
     Torture::Snippet.extract_from(file: file, root: root, marker: section, hide: hide, unindent: true)
